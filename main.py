77b26dc39d3735f596c54e8230d4c29412f90d60
CLSigmaaa <valentin.longagnani@estiam.com>
2024-01-23 13:00:21 +0100

    enhancement: added dda algorithm for more fps

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pygame
 import math
 import time
+import numpy as np
 
 WIDTH = 1200
+HALF_WIDTH = WIDTH // 2
 HEIGHT = 700
+HALF_HEIGHT = HEIGHT // 2
 
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
 
 CELL_SIZE = 100
 
-NUM_RAYS = 100
+NUM_RAYS = WIDTH // 5
 DELTA_ANGLE = FOV / NUM_RAYS
 
-SCREEN_DIST = WIDTH / 2*math.tan(HALF_FOV)
-PROJ_COEFF = 2*SCREEN_DIST*CELL_SIZE
+DIST = NUM_RAYS / (2 * math.tan(HALF_FOV))
+PROJ_COEFF = 2 * DIST * CELL_SIZE
 SCALE = WIDTH // NUM_RAYS
 
-MAX_FPS = 600
+MAX_FPS = 300
 
 pygame.init()
 
@@ -34,6 +37,8 @@ map = [
     ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']
 ]
 
+MAX_DEPTH = 20
+
 world_map = set()
 for j, row in enumerate(map):
     for i, char in enumerate(row):
@@ -49,6 +54,10 @@ class Player:
     @property
     def pos(self):
         return self.x, self.y
+
+    @property
+    def map_pos(self):
+        return int(self.x // CELL_SIZE), int(self.y // CELL_SIZE)
     
     def movement(self, dt):
         cos_a = math.cos(self.angle)
@@ -93,29 +102,49 @@ class Player:
 
 player = Player()
 
+def mapping(a, b):
+    return (a // CELL_SIZE) * CELL_SIZE, (b // CELL_SIZE) * CELL_SIZE
+
+
 def ray_cast(player: Player):
     # draw background
     pygame.draw.rect(sc, (0, 255, 0), (0, HEIGHT // 2, WIDTH, HEIGHT // 2))
     pygame.draw.rect(sc, (0, 0, 255), (0, 0, WIDTH, HEIGHT // 2))
     
-    # actual ray casting
-    curr_angle = player.angle - HALF_FOV
-    pos_x, pos_y = player.pos
+    ox, oy = player.pos
+    x_map, y_map = (ox // CELL_SIZE) * CELL_SIZE, (oy // CELL_SIZE) * CELL_SIZE
+    curr_angle = player.angle - HALF_FOV + 1e-6
+    
     for ray in range(NUM_RAYS):
         sin_a = math.sin(curr_angle)
         cos_a = math.cos(curr_angle)
-        for depth in range(int(WIDTH)):
-            x = pos_x + depth * cos_a
-            y = pos_y + depth * sin_a
-            map_x, map_y = int(x // CELL_SIZE), int(y // CELL_SIZE)
-            if (0 <= map_x < len(map[0])) and (0 <= map_y < len(map)):
-                if map[map_y][map_x] == 'W':
-                    depth *= math.cos(player.angle - curr_angle) # fix the fisheye effect
-                    proj_height = PROJ_COEFF / (depth + 0.00001)
-                    c = 255 / (1 + depth * depth * 0.00001)
-                    color = (63 + c // 2, 63 + c // 2, 63 +c // 2)
-                    pygame.draw.rect(sc, color, (ray * SCALE, (HEIGHT // 2) - proj_height // 2, SCALE, proj_height ))
-                    break
+        
+        # vertical line
+        x, dx = (x_map + CELL_SIZE, 1) if cos_a >= 0 else (x_map, -1)
+        for _ in range(0, WIDTH, CELL_SIZE):
+            depth_v = (x - ox) / cos_a
+            y = oy + depth_v * sin_a
+            if mapping(x + dx, y) in world_map:
+                break
+            x += dx * CELL_SIZE
+        
+        # horizontal line
+        y, dy = (y_map + CELL_SIZE, 1) if sin_a >= 0 else (y_map, -1)
+        for _ in range(0, HEIGHT, CELL_SIZE):
+            depth_h = (y - oy) / sin_a
+            x = ox + depth_h * cos_a
+            if mapping(x, y + dy) in world_map:
+                break
+            y += dy * CELL_SIZE
+            
+            
+        depth = depth_v if depth_v < depth_h else depth_h
+        depth *= math.cos(player.angle - curr_angle)
+        proj_height = PROJ_COEFF / depth
+        c = 255 / (1 + depth * depth * 0.00001)
+        color = (63 + c // 2, 63 + c // 2, 63 +c // 2)
+        pygame.draw.rect(sc, color, (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
+
         curr_angle += DELTA_ANGLE
 
 def no_ray_cast(player: Player):
@@ -156,10 +185,15 @@ while True:
     text_fps = font.render('FPS : ' + str(int(clock.get_fps())), True, pygame.Color('white'))
     text_x = font.render('Player X:' + str(int(player.x)), True, pygame.Color('white'))
     text_y = font.render('Player Y: ' + str(int(player.y)), True, pygame.Color('white'))
+    map_pos = player.map_pos
+    text_map_x = font.render('Player Map Pos X: ' + str(map_pos[0]), True, pygame.Color('white'))
+    text_map_y = font.render('Player Map Pos Y: ' + str(map_pos[1]), True, pygame.Color('white'))
     text_angle = font.render('Player Angle : ' + str(int(math.degrees(player.angle) % 360)), True, pygame.Color('white'))
     sc.blit(text_fps, (50, 50))
     sc.blit(text_x, (50, 100))
     sc.blit(text_y, (50, 150))
     sc.blit(text_angle, (50, 200))
+    sc.blit(text_map_x, (50, 250))
+    sc.blit(text_map_y, (50, 300))
     
     pygame.display.flip()
