28424afc12ef58911d68ab4de645f9db80509c4f
CLSigmaaa <valentin.longagnani@estiam.com>
2024-01-24 17:08:16 +0100

    chores: added comments to the ray casting function
    Also added textures for later texture mapping

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import time
 import numpy as np
 import random
 
-WIDTH = 1920
+WIDTH = 1200
 HALF_WIDTH = WIDTH // 2
-HEIGHT = 1080
+HEIGHT = 700
 HALF_HEIGHT = HEIGHT // 2
 
 FOV = math.pi / 3
@@ -30,41 +30,50 @@ MAX_FPS = 144
 MAX_MAP_SIZE_WIDTH = WIDTH // CELL_SIZE
 MAX_MAP_SIZE_HEIGHT = HEIGHT // CELL_SIZE
 
+TEXTURE_SIZE = 256
+HALF_TEXTURE_SIZE = TEXTURE_SIZE // 2
+
 
 pygame.init()
+
+wall_texture = pygame.image.load('./ressources/textures/1.png')
+
+textures = {
+    'W': wall_texture
+}
 
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-# map = [
-#     ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
-#     ['W', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W'],
-#     ['W', '.', '.', 'W', 'W', 'W', '.', '.', '.', 'W', '.', 'W'],
-#     ['W', 'W', '.', '.', 'W', '.', '.', '.', '.', '.', '.', 'W'],
-#     ['W', '.', '.', '.', '.', '.', '.', 'W', '.', '.', '.', 'W'],
-#     ['W', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W'],
-#     ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']
-# ]
+map = [
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['W', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W'],
+    ['W', '.', '.', 'W', 'W', 'W', '.', '.', '.', 'W', '.', 'W'],
+    ['W', 'W', '.', '.', 'W', '.', '.', '.', '.', '.', '.', 'W'],
+    ['W', '.', '.', '.', '.', '.', '.', 'W', '.', '.', '.', 'W'],
+    ['W', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'W'],
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']
+]
 
-def generate_basic_map(width, height, cell_size):
-    max_map_width = width // cell_size
-    max_map_height = height // cell_size
+# def generate_basic_map(width, height, cell_size):
+#     max_map_width = width // cell_size
+#     max_map_height = height // cell_size
     
-    map = []
-    for y in range(max_map_height):
-        row = []
-        for x in range(max_map_width):
-            if x == 0 or x == max_map_width - 1 or y == 0 or y == max_map_height - 1:
-                row.append('W')
-            else:
-                if random.random() < 0.2:  # Adjust the probability as needed
-                    row.append('W')
-                else:
-                    row.append('.')
-        map.append(row)
-    return map
+#     map = []
+#     for y in range(max_map_height):
+#         row = []
+#         for x in range(max_map_width):
+#             if x == 0 or x == max_map_width - 1 or y == 0 or y == max_map_height - 1:
+#                 row.append('W')
+#             else:
+#                 if random.random() < 0.2:  # Adjust the probability as needed
+#                     row.append('W')
+#                 else:
+#                     row.append('.')
+#         map.append(row)
+#     return map
 
-map = generate_basic_map(WIDTH, HEIGHT, CELL_SIZE)
+# map = generate_basic_map(WIDTH, HEIGHT, CELL_SIZE)
 
 MAX_DEPTH = 20
 
@@ -139,7 +148,7 @@ def mapping(a, b):
 def mini_mapping(a, b):
     return (a // MINI_MAP_TILE) * MINI_MAP_TILE, (b // MINI_MAP_TILE) * MINI_MAP_TILE
 
-def display_mini_mpa():
+def display_mini_map():
     def mini_mapping(a, b):
         return (a // MINI_MAP_TILE) * MINI_MAP_TILE, (b // MINI_MAP_TILE) * MINI_MAP_TILE
     
@@ -150,59 +159,15 @@ def display_mini_mpa():
     mini_map_surface.set_alpha(300)
     
     # draw the player
-    pygame.draw.circle(mini_map_surface, pygame.Color('white'), (int(player.x // MINI_MAP_SCALE), int(player.y // MINI_MAP_SCALE)), 5)
+    pygame.draw.circle(mini_map_surface, pygame.Color('red'), (int(player.x // MINI_MAP_SCALE), int(player.y // MINI_MAP_SCALE)), 5)
     
     # draw the fov
-    pygame.draw.line(mini_map_surface, pygame.Color('white'), (player.x // MINI_MAP_SCALE, player.y // MINI_MAP_SCALE), ((player.x + WIDTH // MINI_MAP_SCALE * math.cos(player.angle - HALF_FOV)) // MINI_MAP_SCALE, (player.y + WIDTH // MINI_MAP_SCALE * math.sin(player.angle - HALF_FOV)) // MINI_MAP_SCALE), 2)
-    pygame.draw.line(mini_map_surface, pygame.Color('white'), (player.x // MINI_MAP_SCALE, player.y // MINI_MAP_SCALE), ((player.x + WIDTH // MINI_MAP_SCALE * math.cos(player.angle + HALF_FOV)) // MINI_MAP_SCALE, (player.y + WIDTH // MINI_MAP_SCALE * math.sin(player.angle + HALF_FOV)) // MINI_MAP_SCALE), 2)
+    # pygame.draw.line(mini_map_surface, pygame.Color('white'), (player.x // MINI_MAP_SCALE, player.y // MINI_MAP_SCALE), ((player.x + WIDTH // MINI_MAP_SCALE * math.cos(player.angle - HALF_FOV)) // MINI_MAP_SCALE, (player.y + WIDTH // MINI_MAP_SCALE * math.sin(player.angle - HALF_FOV)) // MINI_MAP_SCALE), 2)
+    # pygame.draw.line(mini_map_surface, pygame.Color('white'), (player.x // MINI_MAP_SCALE, player.y // MINI_MAP_SCALE), ((player.x + WIDTH // MINI_MAP_SCALE * math.cos(player.angle + HALF_FOV)) // MINI_MAP_SCALE, (player.y + WIDTH // MINI_MAP_SCALE * math.sin(player.angle + HALF_FOV)) // MINI_MAP_SCALE), 2)
     
     # draw the walls
     for x, y in mini_map:
         pygame.draw.rect(mini_map_surface, (255, 255, 255), (x, y, MINI_MAP_TILE, MINI_MAP_TILE), 2)
-    
-    # ray casting for mini map
-    ox = player.x // MINI_MAP_SCALE
-    oy = player.y // MINI_MAP_SCALE
-    x_map, y_map = mini_mapping(ox, oy)
-    
-    curr_angle = player.angle - HALF_FOV + 1e-6
-    for ray in range(NUM_RAYS):
-        sin_a = math.sin(curr_angle)
-        cos_a = math.cos(curr_angle)
-        
-        # vertical line
-        x, dx = (x_map + MINI_MAP_TILE, 1) if cos_a >= 0 else (x_map, -1)
-        for _ in range(0, WIDTH // MINI_MAP_SCALE, MINI_MAP_TILE):
-            depth_v = (x - ox) / cos_a
-            y = oy + depth_v * sin_a
-            # pygame.draw.line(mini_map_surface, pygame.Color('red'), (ox, oy), (x, y))
-            if mini_mapping(x + dx, y) in mini_map:
-                # draw the ray
-                # pygame.draw.line(mini_map_surface, pygame.Color('red'), (ox, oy), (x, y))
-                break
-            x += dx * MINI_MAP_TILE
-        
-        # horizontal line
-        y, dy = (y_map + MINI_MAP_TILE, 1) if sin_a >= 0 else (y_map, -1)
-        for _ in range(0, HEIGHT // MINI_MAP_SCALE, MINI_MAP_TILE):
-            depth_h = (y - oy) / sin_a
-            x = ox + depth_h * cos_a
-            # pygame.draw.line(mini_map_surface, pygame.Color('red'), (ox, oy), (x, y))
-            if mini_mapping(x, y + dy) in mini_map:
-                # draw the ray
-                # pygame.draw.line(mini_map_surface, pygame.Color('red'), (ox, oy), (x, y))
-                break
-            y += dy * MINI_MAP_TILE
-            
-            
-        depth = depth_v if depth_v < depth_h else depth_h
-        
-        # draw the ray
-        pygame.draw.line(mini_map_surface, pygame.Color('red'), (ox, oy), (x, y))
-        
-        curr_angle += DELTA_ANGLE
-        
-        
 
     # Draw the mini map on the screen
     sc.blit(mini_map_surface, MINI_MAP_POS)
@@ -214,24 +179,43 @@ def ray_cast(player: Player):
     pygame.draw.rect(sc, (0, 255, 0), (0, HEIGHT // 2, WIDTH, HEIGHT // 2))
     pygame.draw.rect(sc, (0, 0, 255), (0, 0, WIDTH, HEIGHT // 2))
     
+    # On récupère la position du joueur
     ox, oy = player.pos
+    # On récupère la cellule dans laquelle se trouve le joueur
     x_map, y_map = (ox // CELL_SIZE) * CELL_SIZE, (oy // CELL_SIZE) * CELL_SIZE
+    # On récupère l'angle de vue du joueur en commencant par l'angle le plus à gauche
     curr_angle = player.angle - HALF_FOV + 1e-6
     
+    # On itère le nombre de rayons
     for ray in range(NUM_RAYS):
+        # On calcule le cosinus et le sinus de l'angle actuel
         sin_a = math.sin(curr_angle)
         cos_a = math.cos(curr_angle)
         
-        # vertical line
+        # On regarde si le rayon rentre en collision avec une ligne verticale de la grid
+        # Si le cosinus est positif, on regarde à droite, sinon à gauche
         x, dx = (x_map + CELL_SIZE, 1) if cos_a >= 0 else (x_map, -1)
+        
+        # On calcule la profondeur du rayon
         for _ in range(0, WIDTH, CELL_SIZE):
+            # depth_v est l'hypothénuse du triangle rectangle formé par le rayon et la ligne verticale
+            # sachant que cos_a = adjacent / hypotenuse, on peut calculer l'hypothénuse
+            # adjacent = x - ox
+            # donc hypotenuse = adjacent / cos_a
             depth_v = (x - ox) / cos_a
+            
+            # On calcule la position y du rayon
             y = oy + depth_v * sin_a
+            
+            # On regarde si la ligne verticale avec laquelle on a fait la collision est dans la world_map
+            # C'est-à-dire si elle est un mur
             if mapping(x + dx, y) in world_map:
+                # dans ce cas, on sort de la boucle
                 break
+            # Sinon, on continue de regarder la ligne verticale suivante
             x += dx * CELL_SIZE
         
-        # horizontal line
+        # Même chose pour les lignes horizontales
         y, dy = (y_map + CELL_SIZE, 1) if sin_a >= 0 else (y_map, -1)
         for _ in range(0, HEIGHT, CELL_SIZE):
             depth_h = (y - oy) / sin_a
@@ -240,19 +224,36 @@ def ray_cast(player: Player):
                 break
             y += dy * CELL_SIZE
             
-            
+        
+        # On calcule la profondeur du mur le plus proche
         depth = depth_v if depth_v < depth_h else depth_h
+        
+        # On corrige la profondeur pour éviter le fish eye effect
+        # Les rayons au centre de l'écran sont plus proches du joueur que ceux sur les côtés
+        # Donc on corrige la profondeur en multipliant par le cosinus de l'angle entre le rayon et le joueur
         depth *= math.cos(player.angle - curr_angle)
+        # Pour éviter les divisions par 0, on ajoute 1e-6 si la profondeur est égale à 0
         if depth == 0:
             depth = 1e-6
+        
+        # On calcule la hauteur du mur
+        # la hauteur projetée est égale à la distance du mur * la taille d'une cellule
         proj_height = PROJ_COEFF / depth
+        
+        # On calcule la couleur du mur
+        # Plus le mur est loin, plus il est sombre
         c = 255 / (1 + depth * depth * 0.00001)
         color = (63 + c // 2, 63 + c // 2, 63 +c // 2)
+        
+        # On dessine le mur
+        # le x du mur est le rayon actuel * la largeur d'un rayon
+        # le y du mur est la moitié de la hauteur de l'écran - la moitié de la hauteur du mur
+        # la largeur du mur est la largeur d'un rayon
+        # la hauteur du mur est la hauteur du mur projetée
         pygame.draw.rect(sc, color, (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
 
+        # On passe au rayon suivant en incrémentant l'angle de DELTA_ANGLE
         curr_angle += DELTA_ANGLE
-
-
 
 
 prev_time = time.time()
@@ -272,16 +273,8 @@ while True:
     # no_ray_cast(player)
     ray_cast(player)
     
+    display_mini_map()
 
-    
-
-
-
-   
-
-
-
-    
     # Debug info
     # font = pygame.font.SysFont('Arial', 20, bold=True)
     # text_fps = font.render('FPS : ' + str(int(clock.get_fps())), True, pygame.Color('white'))
